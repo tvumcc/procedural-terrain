c0bb8ef8f203f9dd7a47ee641cd86b8122853bdd
Thomas McConkey <tvmcconkey@gmail.com>
2023-04-10 17:51:09 -0500

    fixed glfw include path

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
 #include <glad/glad.h>
-#include <glfw/glfw3.h>
+#include <GLFW/glfw3.h>
 #include <stb_image/stb_image.h>
 
 #include "shader.hpp"
